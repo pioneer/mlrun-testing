5cc85d26e2b8c3cb956c3cecb4fb68304dd7a7b4
Serhii Tarkovskyi <serhii.tarkovskyi@ihsmarkit.com>
2021-04-29 21:24:54 +0300

    Replaced numbers with reading a simple text file

--- functions/step1.py
+++ functions/step1.py
@@ -1,11 +1,15 @@
+from mlrun import get_dataitem
 from .utils import MultipleOutputStep
 
 
 class Step1(MultipleOutputStep):
 
     def get_data_iter(self, data):
-        yield from range(data)
+        for line in self.file.get().splitlines():
+            yield line
 
     async def do(self, data):
         self.context.logger.info(f"Step 1: received {data}")
+        self.file = get_dataitem(f"./artifacts/{data}")
+        self.context.logger.info(f"Step 1: file {self.file}")
         return await super().do(data)
--- functions/step2.py
+++ functions/step2.py
@@ -5,6 +5,6 @@ class Step2:
     def __init__(self, context, **kwargs):
         self.context = context
 
-    def do(self, number):
-        self.context.logger.info(f"Step 2: received {number}")
-        return number*10
+    def do(self, data):
+        self.context.logger.info(f"Step 2: received {data}")
+        return data.capitalize()
